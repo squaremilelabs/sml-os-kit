aef22503eb57f83120572239a0ed611c20f94b2f
E <e@elzr.me>
2024-05-05 15:56:16 -0400

    close nav bar on small screens

--- app-core/admin/layout/parts/NavItems/index.tsx
+++ app-core/admin/layout/parts/NavItems/index.tsx
@@ -3,10 +3,12 @@
 import { AdminNavItem } from "@/~sml-os-kit/config/types"
 import getAdminNavigationForOSUser from "@/~sml-os-kit/modules/auth/functions/getAdminNavigationForOSUser"
 import useAuthState from "@/~sml-os-kit/modules/auth/hooks/useAuthState"
-import { mdiArrowTopRight } from "@mdi/js"
+import { mdiArrowTopRight, mdiCircleOutline } from "@mdi/js"
 import Icon from "@mdi/react"
 import { Accordion, AccordionItem, Button, ScrollShadow, Spacer } from "@nextui-org/react"
 import Link from "next/link"
+import useNavState from "../useNavState"
+import useScreenSize from "@/~sml-os-kit/common/hooks/useScreenSize"
 
 export default function NavItems() {
   const auth = useAuthState()
@@ -28,11 +30,9 @@ export default function NavItems() {
 }
 
 function NavGroup({ navItem }: { navItem: AdminNavItem }) {
-  const startContent = navItem.iconPath ? (
-    <Icon path={navItem.iconPath} className="w-4" />
-  ) : (
-    <Spacer className="w-4" />
-  )
+  const { navOpen, setNavOpen } = useNavState()
+  const screenSize = useScreenSize()
+  const handlePress = navOpen && screenSize !== "lg" ? () => setNavOpen(false) : undefined
   return (
     <Accordion className="p-0 px-0 py-0">
       <AccordionItem
@@ -45,7 +45,7 @@ function NavGroup({ navItem }: { navItem: AdminNavItem }) {
         }}
         key={navItem.href}
         title={navItem.label}
-        startContent={startContent}
+        startContent={<Icon path={navItem.iconPath ?? mdiCircleOutline} className="w-4" />}
         isCompact
       >
         {navItem.items?.map((innerNavItem) => {
@@ -56,6 +56,7 @@ function NavGroup({ navItem }: { navItem: AdminNavItem }) {
               href={innerNavItem.href}
               className="justify-between rounded-sm h-fit p-1 w-full pl-4 truncate"
               variant="light"
+              onPress={handlePress}
             >
               <span className="grow text-left">{innerNavItem.label}</span>
               {innerNavItem.type === "link" ? (
@@ -70,18 +71,18 @@ function NavGroup({ navItem }: { navItem: AdminNavItem }) {
 }
 
 function NavItem({ navItem }: { navItem: AdminNavItem }) {
-  const startContent = navItem.iconPath ? (
-    <Icon path={navItem.iconPath} className="w-4 mr-2" />
-  ) : (
-    <Spacer className="w-4 mr-2" />
-  )
+  const { navOpen, setNavOpen } = useNavState()
+  const screenSize = useScreenSize()
+  const handlePress = navOpen && screenSize !== "lg" ? () => setNavOpen(false) : undefined
+
   return (
     <Button
       as={Link}
       href={navItem.href}
       className="rounded-none gap-0 text-foreground text-sm justify-start h-9 min-h-9 border-b-1 border-default-200 p-1 truncate"
       variant="light"
-      startContent={startContent}
+      startContent={<Icon path={navItem.iconPath ?? mdiCircleOutline} className="w-4 mr-2" />}
+      onPress={handlePress}
     >
       <span className="grow text-left">{navItem.label}</span>
       {navItem.type === "link" ? <Icon path={mdiArrowTopRight} className="w-3" /> : null}
