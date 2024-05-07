15e4e1e1e816bbce33879a40fbb08671e01f9e4e
E <e@elzr.me>
2024-05-07 16:05:44 -0400

    remove viewport config

--- app-core/root/layout/viewport.ts
+++ app-core/root/layout/viewport.ts
@@ -3,8 +3,6 @@ import type { Viewport } from "next"
 const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
-  viewportFit: "cover",
 }
 
 export default viewport
