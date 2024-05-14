81c9fec677eded40b4ab85096a54317ebddd0b47
David Roghanian <droghanian@gmail.com>
2024-05-14 11:17:38 -0400

    _createTicket function

---
+++ modules/roadmap/functions/_createTicket.ts
@@ -0,0 +1,44 @@
+"use server"
+import { RoadmapItem } from "@/~sml-os-kit/modules/roadmap/types"
+import { getSiteConfig } from "../../../config/functions"
+import { Client } from "@notionhq/client"
+
+export default async function _createTicket(
+  input: Pick<RoadmapItem, "title" | "description" | "urgent" | "creatorEmail">
+) {
+  const notion = new Client({ auth: process.env.NOTION_TOKEN })
+  const ticketsDatabaseId = getSiteConfig().integrations.notion.ticketsDatabaseId
+  if (!ticketsDatabaseId) throw new Error("Tickets database not found")
+
+  const properties = {
+    "Title": {
+      title: [
+        {
+          text: {
+            content: input.title,
+          },
+        },
+      ],
+    },
+    "Description": {
+      rich_text: [
+        {
+          text: {
+            content: input.description,
+          },
+        },
+      ],
+    },
+    "Creator Email": { email: input.creatorEmail },
+  } as Record<string, any>
+
+  if (input.urgent) {
+    properties["Urgent"] = { select: { name: "Urgent" } }
+  }
+
+  const response = await notion.pages.create({
+    parent: { database_id: ticketsDatabaseId },
+    properties,
+  })
+  return response
+}
