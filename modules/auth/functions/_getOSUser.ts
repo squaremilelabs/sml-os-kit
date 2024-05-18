9e69e8e80004bab328e1089861bd2e9d0d9323de
E <e@elzr.me>
2024-05-18 16:23:23 -0400

    disable getOSUser fetch on demo mode

--- modules/auth/functions/_getOSUser.ts
+++ modules/auth/functions/_getOSUser.ts
@@ -1,8 +1,11 @@
+"use server"
+
 import _getUser from "@/~sml-os-kit/~sml-firebase/auth/user-functions/_getUser"
 import { BaseOSUser, OSUser } from "../types"
 import roles from "@/$sml-os-config/roles"
 
 export default async function _getOSUser(userId: string): Promise<OSUser | null> {
+  if (process.env.OS_DEMO_MODE) return null
   const baseUser = await _getUser<BaseOSUser>(userId)
   const role = roles.find((role) => role.id === baseUser?.roleId)
   if (!role) return null
