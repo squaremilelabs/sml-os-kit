3772bba44c228fa2d31dab6e23bb5f1dbc18e756
E <e@elzr.me>
2024-05-18 13:56:22 -0400

    update email layout

--- modules/emails/components/base/OSEmailLayout.tsx
+++ modules/emails/components/base/OSEmailLayout.tsx
@@ -15,13 +15,13 @@ export default function OSEmailLayout({
       <Body className="bg-zinc-100 font-sans py-[24px] px-[8px]">
         <Container className="bg-white rounded border border-solid border-zinc-200">
           <Section className="px-[16px] py-[8px] border-b border-solid border-zinc-200">
-            <Text className="text-xl text-primary font-semibold">{brandConfig.orgName}</Text>
+            <Text className="text-xl text-primary font-semibold">{brandConfig.appName}</Text>
           </Section>
           <Section className="min-h-[200px]">{children}</Section>
           <Section className="bg-primary px-[8px] rounded-bl rounded-br">
             <Row>
               <Column align="center">
-                <Text className="text-primary-100 mx-auto">{brandConfig.appName}</Text>
+                <Text className="text-primary-100 mx-auto">{brandConfig.orgName}</Text>
               </Column>
             </Row>
           </Section>
