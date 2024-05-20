8683626b61fa9df3c73b3ce304b90fa4d821c7e4
E <e@elzr.me>
2024-05-20 10:59:13 -0400

    setup email param in portal wrapper

--- app-core/portal/layout/parts/PortalWrapper.tsx
+++ app-core/portal/layout/parts/PortalWrapper.tsx
@@ -4,6 +4,7 @@ import _queryOSUsers from "@/~sml-os-kit/auth/functions/_queryOSUsers"
 import useAuthState from "@/~sml-os-kit/auth/hooks/useAuthState"
 import BrandLogotype from "@/~sml-os-kit/common/components/BrandLogotype"
 import _runSafeServerAction from "@/~sml-os-kit/common/functions/_runSafeServerAction"
+import usePageRouter from "@/~sml-os-kit/common/hooks/usePageRouter"
 import usePortal from "@/~sml-os-kit/modules/portal-utils/usePortal"
 import {
   Button,
@@ -15,12 +16,22 @@ import {
   ModalHeader,
 } from "@nextui-org/react"
 import { useMutation } from "@tanstack/react-query"
-import React, { useState } from "react"
+import { useSearchParams } from "next/navigation"
+import React, { useLayoutEffect, useState } from "react"
 
 export default function PortalWrapper({ children }: { children: React.ReactNode }) {
+  const searchParams = useSearchParams()
+  const router = usePageRouter()
   const auth = useAuthState()
   const [email, setEmail] = useState("")
   const { portal, portalUser, setPortalUser } = usePortal()
+
+  const emailParam = searchParams.get("email")
+  useLayoutEffect(() => {
+    if (emailParam) {
+      setEmail(emailParam)
+    }
+  }, [emailParam])
 
   const modalIsOpen = auth.user?.role?.type === "console" && !portalUser
 
@@ -28,6 +39,7 @@ export default function PortalWrapper({ children }: { children: React.ReactNode 
     mutationKey: ["portalWrapper", email],
     mutationFn: async () => {
       if (auth?.user?.roleId === "demo") {
+        router.push(null, { email: null }, { merge: true })
         setPortalUser({ ...auth.user, email })
         return
       }
@@ -39,6 +51,7 @@ export default function PortalWrapper({ children }: { children: React.ReactNode 
       if (!selectedUser) throw new Error("User not found")
       if (selectedUser.role?.type !== "portal") throw new Error(`Not a ${portal?.title} user`)
       if (selectedUser.role.portalId !== portal?.id) throw new Error(`Not a ${portal?.title} user`)
+      router.push(null, { email: null }, { merge: true })
       setPortalUser(selectedUser)
     },
   })
